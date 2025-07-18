e14e83254d2b3f4f220c0c9d36aacc8d8fca8012
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-07-18 18:20:30 -0400

    * Created components to display a list of notes, add a new note and filter only 'important' notes.
    * Add App.css and index.css for improved styling and layout.
    
    Signed-off-by: Ethan Anderson <150651098+mite404@users.noreply.github.com>

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,14 +1,53 @@
 import Note from './components/Note'
+import {useState} from "react";
+import './App.css'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {  // create new object for the incoming note from component's newNote state
+      content: newNote,  // value of 'newNote' will be passed here from user's input
+      important: Math.random() < 0.5,  // 50% chance of being marked 'important'
+      id: String(notes.length + 1),  // unique identifier generated based on total number of notes
+    }
+
+    setNotes(notes.concat(noteObject))  // add new note to list of notes -- creates new copy of
+    // array
+    setNewNote('')  // event handler resets value of controlled input
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+      ? notes
+      : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input  value={newNote}
+                onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
